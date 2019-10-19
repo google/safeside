4926cc471c4b619752aad55b84e2795288dc6736
Anthony Steinhauser <35879957+asteinha@users.noreply.github.com>
2019-10-18 19:05:54 -0700

    Meltdown-SS for AMD. (#44)

--- demos/instr.h
+++ demos/instr.h
@@ -84,11 +84,48 @@ inline void JumpToAfterSpeculation() {
   asm volatile("b afterspeculation");
 }
 #endif
-#if defined(__i386__) || defined(__x86_64__) || defined(_M_X64) || \
-    defined(_M_IX86)
+
+#if defined(__i386__) || defined(__x86_64__)
+// Yields serializing instruction.
+// Must be inlined in order to avoid mispredicted Spectre v2 jumps over it.
 __attribute__((always_inline))
 inline void MemoryAndSpeculationBarrier() {
-  asm volatile("cpuid"::"a"(0):"ebx", "ecx", "edx");
+  asm volatile("cpuid"::"a"(0):"ebx", "ecx", "edx", "memory");
+}
+#endif
+
+#ifdef __i386__
+// Returns the original value of FS and sets the new value.
+int ExchangeFS(int input);
+// Returns the original value of ES and sets the new value.
+int ExchangeES(int input);
+
+// Reads an offset from the FS segment.
+// Must be inlined because the fault occurs inside and the stack pointer would
+// be shifted.
+__attribute__((always_inline))
+inline unsigned int ReadUsingFS(unsigned int offset) {
+  unsigned int result;
+
+  asm volatile(
+      "movzbl %%fs:(, %1, 1), %0\n"
+      :"=r"(result):"r"(offset):"memory");
+
+  return result;
+}
+
+// Reads an offset from the ES segment.
+// Must be inlined because the fault occurs inside and the stack pointer would
+// be shifted.
+__attribute__((always_inline))
+inline unsigned int ReadUsingES(unsigned int offset) {
+  unsigned int result;
+
+  asm volatile(
+      "movzbl %%es:(, %1, 1), %0\n"
+      :"=r"(result):"r"(offset):"memory");
+
+  return result;
 }
 #endif
 #endif
