a8853a37f22fe34be246545323750dc07b406302
Anthony Steinhauser <asteinhauser@google.com>
2019-10-22 10:26:41 -0700

    WIP. Testing why Travis does not build Meltdown and Foreshadow with PowerPC.

--- ci/toolchains/powerpc64le-linux-gnu.cmake
+++ ci/toolchains/powerpc64le-linux-gnu.cmake
@@ -1,6 +1,6 @@
 set(CMAKE_SYSTEM_NAME Linux)
 
-set(CMAKE_SYSTEM_PROCESSOR powerpc64le)
+set(CMAKE_SYSTEM_PROCESSOR powerpc64)
 
 set(CMAKE_C_COMPILER powerpc64le-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER powerpc64le-linux-gnu-g++)
