9e80f587876de64c603c287ea96d7aab0cbf60be
Anthony Steinhauser <asteinhauser@google.com>
2019-10-23 06:06:18 -0700

    Using the utility function.

--- demos/ret2spec_cyclic.cc
+++ demos/ret2spec_cyclic.cc
@@ -92,11 +92,7 @@ static bool ReturnsTrue(int counter) {
   // own stack mark and the next one. Somewhere there must be also the return
   // address.
   stack_mark_pointers.pop_back();
-  for (int i = 0; i < (stack_mark_pointers.back() - &stack_mark);
-       i += kCacheLineSize) {
-    CLFlush(&stack_mark + i);
-  }
-  CLFlush(stack_mark_pointers.back());
+  FlushFromCache(&stack_mark, stack_mark_pointers.back());
   return true;
 }
 
