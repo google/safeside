bd6c3d5e7472207fc8c2b28675816af51575e9fd
Anthony Steinhauser <asteinhauser@google.com>
2019-11-07 16:17:24 -0800

    Readding include guards to local_content.h

--- demos/local_content.h
+++ demos/local_content.h
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+#ifndef DEMOS_LOCAL_CONTENT_H
+#define DEMOS_LOCAL_CONTENT_H
 // Generic strings used across examples. The public_data is intended to be
 // accessed in the C++ execution model. The content of the private_data is
 // intended to be leaked outside of the C++ execution model using sidechannels.
@@ -21,3 +23,4 @@
 // demonstrating.
 const char *public_data = "Hello, world!";
 const char *private_data = "It's a s3kr3t!!!";
+#endif  // DEMOS_LOCAL_CONTENT_H
