f033368565d1f286236dd103dac0a1db7b7180a2
Matthew Riley <mattdr@google.com>
2020-05-25 23:41:31 -0700

    Rewrite meltdown_module to use debugfs
    Sort of gratuitous renames of `private_data` to `secret_data`

--- kernel_modules/kmod_meltdown/meltdown_module.c
+++ kernel_modules/kmod_meltdown/meltdown_module.c
@@ -7,6 +7,7 @@
  * SPDX-License-Identifier: BSD-3-Clause OR GPL-2.0
  */
 
+#include <linux/debugfs.h>
 #include <linux/module.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
@@ -14,7 +15,7 @@
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Google");
 MODULE_DESCRIPTION("");
-MODULE_VERSION("0.1");
+MODULE_VERSION("0.2");
 
 // Keeps a buffer in kernel memory and makes its address available at:
 //   /proc/safeside_meltdown/address
@@ -24,79 +25,43 @@ MODULE_VERSION("0.1");
 
 // Secret data stored in the kernel memory whose content is never directly
 // leaked through sysfs.
-const char *private_data = "It's a s3kr3t!!!";
+static const char secret_data[] = "It's a s3kr3t!!!";
+static const int secret_data_length = sizeof(secret_data);
 
-// Directory record. Must be available on unloading the module.
-struct proc_dir_entry *safeside_meltdown;
-
-// Print the address of `private_data` into the provided buffer.
-static int address_show(struct seq_file *file, void *v) {
-  seq_printf(file, "%px\n", private_data);
-  return 0;
-}
-
-// Print the length of `private_data` into the sequential file.
-// At the same time loads `private_data` to cache by accessing it.
-static int length_show(struct seq_file *file, void *v) {
-  seq_printf(file, "%d\n", (int) strlen(private_data));
-  return 0;
-}
-
-static int address_open(struct inode *i, struct file *file) {
-  return single_open(file, address_show, NULL);
-}
-
-static int length_open(struct inode *i, struct file *file) {
-  return single_open(file, length_show, NULL);
-}
-
-static struct file_operations address_file_ops = {
-  .open = address_open,
-  .read = seq_read,
-  .llseek = seq_lseek,
-  .release = single_release,
-};
-
-static struct file_operations length_file_ops = {
-  .open = length_open,
-  .read = seq_read,
-  .llseek = seq_lseek,
-  .release = single_release,
-};
+static struct dentry *root_dir = NULL;
 
 static int __init meltdown_init(void) {
-  struct proc_dir_entry *address, *length;
+  struct dentry *child = NULL;
 
-  pr_info("safeside_meltdown init\n");
-
-  safeside_meltdown = proc_mkdir("safeside_meltdown", NULL);
-  if (safeside_meltdown == NULL) {
-    return -ENOMEM;
+  root_dir = debugfs_create_dir("safeside_meltdown", /*parent=*/NULL);
+  if (!root_dir) {
+    goto err;
   }
 
-  // Read-only files, accessible only by root.
-  address = proc_create("address", 0400, safeside_meltdown, &address_file_ops);
-  if (address == NULL) {
-    remove_proc_entry("safeside_meltdown", NULL);
-    return -ENOMEM;
+  child = debugfs_create_u32("secret_data_length",
+      0400, root_dir, (u32*)&secret_data_length);
+  if (!child) {
+    goto err;
   }
 
-  length = proc_create("length", 0400, safeside_meltdown, &length_file_ops);
-  if (length == NULL) {
-    remove_proc_entry("address", safeside_meltdown);
-    remove_proc_entry("safeside_meltdown", NULL);
-    return -ENOMEM;
+  child = debugfs_create_x64("secret_data_address",
+      0400, root_dir, (u64*)&secret_data);
+  if (!child) {
+    goto err;
   }
 
   return 0;
+
+err:
+  debugfs_remove_recursive(root_dir);  /* benign on NULL */
+  root_dir = NULL;
+
+  return -ENODEV;
 }
 
 static void __exit meltdown_exit(void) {
-  pr_info("safeside_meltdown exit\n");
-
-  remove_proc_entry("length", safeside_meltdown);
-  remove_proc_entry("address", safeside_meltdown);
-  remove_proc_entry("safeside_meltdown", NULL);
+  debugfs_remove_recursive(root_dir);
+  root_dir = NULL;
 }
 
 module_init(meltdown_init);
