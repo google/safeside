b9dbc6fcaf33a1ccda11c383c21cf5cb637e24aa
Matthew Riley <mattdr@google.com>
2020-05-28 18:45:38 -0700

    Add a new RunWithFaultHandler utility
    This avoids the need for a global `afterspeculation` label.
    
    Use to rewrite the Meltdown example.

---
+++ demos/faults.cc
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under both the 3-Clause BSD License and the GPLv2, found in the
+ * LICENSE and LICENSE.GPL-2.0 files, respectively, in the root directory.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause OR GPL-2.0
+ */
+
+#include "faults.h"
+
+#include <setjmp.h>
+
+#include <cstring>
+
+namespace {
+
+// The sigjmp context we should jump to after catching the fault.
+sigjmp_buf signal_handler_jmpbuf;
+
+extern "C"
+void SignalHandler(int signal, siginfo_t *info, void *ucontext) {
+  siglongjmp(signal_handler_jmpbuf, 1);
+}
+
+}  // namespace
+
+bool RunWithFaultHandler(std::function<void()> inner) {
+  struct sigaction sa = {};
+  sa.sa_sigaction = SignalHandler;
+
+  struct sigaction oldsa;
+  // This sets the signal handler for the entire process.
+  sigaction(SIGSEGV, &sa, &oldsa);
+
+  // Use sigsetjmp/siglongjmp to save and restore signal mask. Otherwise we
+  // will jump out of the signal handler and leave the currently-being-handled
+  // signal blocked. The result of a SIGSEGV being raised while blocked is
+  // "undefined"[1], but in practice leads to killing the process.
+  //
+  // [1] https://www.man7.org/linux/man-pages/man2/sigprocmask.2.html#NOTES
+  bool handled_fault = true;
+  if (sigsetjmp(signal_handler_jmpbuf, 1) == 0) {
+    inner();
+    handled_fault = false;
+  }
+
+  sigaction(SIGSEGV, &oldsa, nullptr);
+
+  return handled_fault;
+}
---
+++ demos/faults_test.cc
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under both the 3-Clause BSD License and the GPLv2, found in the
+ * LICENSE and LICENSE.GPL-2.0 files, respectively, in the root directory.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause OR GPL-2.0
+ */
+
+#include "faults.h"
+
+#include <iostream>
+
+// Tests that a SIGSEGV is successfully caught and the handler runs.
+bool TestHandlesSigsegv() {
+  bool pass = true;
+
+  bool ran_body = false;
+  bool saw_fault = RunWithFaultHandler([&]() {
+    ran_body = true;
+    raise(SIGSEGV);
+  });
+
+  if (!ran_body) {
+    std::cerr << "Didn't run expected function" << std::endl;
+    pass = false;
+  }
+  if (!saw_fault) {
+    std::cerr << "Didn't see expected fault" << std::endl;
+    pass = false;
+  }
+
+  return pass;
+}
+
+// Test that the handler doesn't run when no fault occurs.
+bool TestNoFault() {
+  bool pass = true;
+
+  bool ran_body = false;
+  bool saw_fault = RunWithFaultHandler([&]() {
+    ran_body = true;
+  });
+
+  if (!ran_body) {
+    std::cerr << "Didn't run expected function" << std::endl;
+    pass = false;
+  }
+  if (saw_fault) {
+    std::cerr << "Saw unexpected fault" << std::endl;
+    pass = false;
+  }
+
+  return pass;
+}
+
+int main(int argc, char* argv[]) {
+  bool pass = true;
+
+  // Run this test twice to check we reset signal masks correctly.
+  pass = pass && TestHandlesSigsegv();
+  pass = pass && TestHandlesSigsegv();
+
+  pass = pass && TestNoFault();
+
+  std::cout << (pass ? "pass" : "fail") << std::endl;
+
+  return !pass;
+}
