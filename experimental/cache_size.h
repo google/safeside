8953eb80aa9a899ecbd0a6bd975a7b6920c41de8
Devin Jeanpierre <jeanpierreda@google.com>
2020-12-07 16:02:43 -0800

    Style fixes.
    I think we need to delete ShuffledRange in favor of the randomization algorithm used elsewhere in safeside, as this increases cache contention substantially, muddying the waters.
    
    See: https://github.com/google/safeside/blob/main/demos/timing_array.h

--- experimental/cache_size.h
+++ experimental/cache_size.h
@@ -14,33 +14,40 @@
 #include "../demos/instr.h"
 #include "../demos/utils.h"
 
+// Returns a shuffled vector of integers from 0 (inclusive) to n (exclusive).
+// This can be used to randomize access order.
+inline std::vector<size_t> ShuffledRange(size_t n) {
+  std::vector<size_t> numbers;
+  numbers.reserve(n);
+  for (size_t i = 0; i < n; ++i) {
+    numbers.push_back(i);
+  }
+  std::random_device rd;
+  std::mt19937 f(rd());
+  std::shuffle(numbers.begin(), numbers.end(), f);
+  return numbers;
+}
+
 // Determines the maximum latency of reading elements of a vector of size "sz",
 // which is given as input.
 // It reads each element of the vector and measures the time
 // using MeasureReadLatency(). Later this number can be used to determine cache
 // misses and hits
-inline uint64_t FindMaxReadingTime(long sz) {
-  // Generate a random order of accesses to eliminate the impact of hw
-  // prefetchers
-  std::vector<int64_t> accesses(sz);
-  for (int64_t i = 0; i < sz; i++) {
-    accesses.push_back(i);
-  }
-  std::random_device rd;
-  std::mt19937 f(rd());
-  std::shuffle(accesses.begin(), accesses.end(), f);
+inline uint64_t FindMaxReadingTime(size_t buffer_size) {
+  std::vector<size_t> accesses = ShuffledRange(buffer_size);
 
-  std::vector<char> buf(sz);
+  std::vector<char> buffer;
+  buffer.reserve(buffer_size);
 
-  for (int64_t i : accesses) {
-    ForceRead(&buf[i]);
+  for (size_t i : accesses) {
+    ForceRead(&buffer[i]);
   }
 
-  //   Read each element in the same random order and keep track of the slowest
-  //   read.
-  uint64_t max_read_latency = std::numeric_limits<uint64_t>::min();
-  for (int64_t i : accesses) {
-    max_read_latency = std::max(max_read_latency, MeasureReadLatency(&buf[i]));
+  // Read each element in the same random order and keep track of the slowest
+  // read.
+  uint64_t max_read_latency = 0;
+  for (size_t i : accesses) {
+    max_read_latency = std::max(max_read_latency, MeasureReadLatency(&buffer[i]));
   }
 
   return max_read_latency;
@@ -51,24 +58,17 @@ inline uint64_t FindMaxReadingTime(long sz) {
 // It reads the first element based on the order of initial accesses and
 // measures the time using MeasureReadLatency(). Later this number can be used
 // to determine cache misses and hits
-inline uint64_t FindFirstElementReadingTime(long sz) {
-  // Generate a random order of accesses to eliminate the impact of hw
-  // prefetchers
-  std::vector<int64_t> accesses(sz);
-  for (int64_t i = 0; i < sz; i++) {
-    accesses.push_back(i);
-  }
-  std::random_device rd;
-  std::mt19937 f(rd());
-  std::shuffle(accesses.begin(), accesses.end(), f);
+inline uint64_t FindFirstElementReadingTime(size_t buffer_size) {
+  std::vector<size_t> accesses = ShuffledRange(buffer_size);
 
-  std::vector<char> buf(sz);
+  std::vector<char> buffer;
+  buffer.reserve(buffer_size);
 
-  for (int64_t i : accesses) {
-    ForceRead(&buf[i]);
+  for (size_t i : accesses) {
+    ForceRead(&buffer[i]);
   }
 
-  return MeasureReadLatency(&buf[accesses[0]]);
+  return MeasureReadLatency(&buffer[accesses[0]]);
 }
 
 #endif  // EXPERIMENTAL_CACHE_SIZE_H_
