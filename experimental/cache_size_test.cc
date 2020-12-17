3b20a7f7cca9e6cfa306be556a457fda77d6999f
Devin Jeanpierre <jeanpierreda@google.com>
2020-12-17 15:03:09 -0800

    Oops, forgot to add a test.

--- experimental/cache_size_test.cc
+++ experimental/cache_size_test.cc
@@ -2,11 +2,12 @@
 #include "cache_size.h"
 
 #include <iostream>
-// Measure how often measuring time is able to accurately determine cache hits
+#include <vector>
+// Measures how often measuring time is able to accurately determine cache hits
 // and cache misses based on the buffer size under analysis and the cache size
 // it checks whether buffer sizes that fit in cache have less reading time or
 // not
-int main(int argc, char* argv[]) {
+int test_timing() {
   static constexpr long kCacheSize = 8 * 1024 * 1024;
   static constexpr long kIterations = 2;
   static constexpr long kTestSizes = 4;
@@ -46,4 +47,38 @@ int main(int argc, char* argv[]) {
   std::cout << res << "% of tests passed." << std::endl;
   if (res > 99) return 0;
   return 1;
+}
+
+
+// Smoke-tests PermuteInt.
+int test_permuting() {
+  int rv;
+  int permuted = PermuteInt(0, /*max=*/1);
+  if ( permuted != 0) {
+    std::cout << "PermuteInt(0, /*max=*/1) = " << permuted << ", expected 0\n";
+    rv = 1;
+  }
+  for (int i = 0; i < 257; ++i) {
+    int permuted = PermuteInt(i, /*max=*/257);
+    if (permuted >= 257) {
+      std::cout << "PermuteInt(" << i << ", /*max=*/257) = " << permuted << ", expected < 257\n";
+      rv = 1;
+    }
+  }
+  return rv;
+}
+
+
+// TODO: absltest... :(
+
+int main() {
+  int rv = 0;
+  if (test_timing()) {
+    std::cout << "test_timing failed";
+    rv = 1;
+  }
+  if (test_permuting()) {
+    std::cout << "test_permuting failed";
+  }
+  return rv;
 }
